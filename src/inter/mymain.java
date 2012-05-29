6b7ad3f1e3f20209c9e841415dc2a728b89ac19c
YANG <mymailzhong@qq.com>
2012-05-29 15:04:59 +0800

    加了3句话（监听器）

--- src/inter/mymain.java
+++ src/inter/mymain.java
@@ -1,12 +1,14 @@
 package inter;
+
 import back.*;
+
 /**
  * Ö÷·½·¨<br>
  * 
  * @author yyz
  */
 public class mymain {
-	public static void main(String[] args){
-		InterMain myInter=new InterMain();
+	public static void main(String[] args) {
+		InterMain myInter = new InterMain();
 	}
 }
